5a1bbaef0efaf0cec15c8249906cd4f94e397f1e
knightfury16 <suhaibknight@gmail.com>
2025-04-02 22:24:59 +0600

    Added debug keybindings for nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,43 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>", { silent = true, noremap = true })
+
+-- Debugging keybindings start
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end, { desc = "Debug: Start/Continue" })
+vim.keymap.set("n", "<F10>", function()
+  require("dap").step_over()
+end, { desc = "Debug: Step Over" })
+vim.keymap.set("n", "<F11>", function()
+  require("dap").step_into()
+end, { desc = "Debug: Step Into" })
+vim.keymap.set("n", "<F12>", function()
+  require("dap").step_out()
+end, { desc = "Debug: Step Out" })
+vim.keymap.set("n", "<leader>db", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Debug: Toggle Breakpoint" })
+vim.keymap.set("n", "<leader>dB", function()
+  require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "Debug: Conditional Breakpoint" })
+vim.keymap.set("n", "<leader>dl", function()
+  require("dap").run_last()
+end, { desc = "Debug: Run Last" })
+vim.keymap.set("n", "<leader>dt", function()
+  require("dap").terminate()
+end, { desc = "Debug: Terminate" })
+vim.keymap.set("n", "<leader>du", function()
+  require("dapui").toggle()
+end, { desc = "Debug: Toggle UI" })
+
+vim.fn.sign_define(
+  "DapBreakpoint",
+  { text = "🔴", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define("DapLogPoint", { text = "🔵", texthl = "DapLogPoint", linehl = "", numhl = "" })
+vim.fn.sign_define(
+  "DapStopped",
+  { text = "🟢", texthl = "DapStopped", linehl = "DapStoppedLine", numhl = "DapStoppedNum" }
+)
+-- Debugging keybindings end
